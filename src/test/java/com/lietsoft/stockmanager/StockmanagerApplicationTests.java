1fb4f03e410b4ecc01ed84622db69004248df3f4
Juanma Alfonso <juanma.alfonso@gmail.com>
2019-10-11 21:29:58 +0200

    Stock Manager first commit

---
+++ src/test/java/com/lietsoft/stockmanager/StockmanagerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lietsoft.stockmanager;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StockmanagerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
